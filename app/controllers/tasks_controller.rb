99209cc50186cf211ed75c77446a840955e82680
titi noguchi <cristina.noguchi@gmail.com>
2021-08-13 00:14:16 -0400

    corregido

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,44 +1,43 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @tasks = Task.all
   end
 
-  def show; end
+  def show
+  end
 
   def new
     @task = Task.new
   end
 
   def create
-    task = Task.new(tasks_params)
+    task = Task.new(task_params)
     task.save
-
-    redirect_to task_path(task)
+    redirect_to tasks_path
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
-    @task.update(tasks_params)
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
     @task.destroy
-
     redirect_to tasks_path
   end
 
-private
-
-  def tasks_params
-    params.require(:task).permit(:title, :details, :completed)
-  end
+  private
 
   def set_task
     @task = Task.find(params[:id])
   end
 
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #verb url nombredelcontroller nombreaccion
-  get "tasks", to: 'tasks#index'
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
-  get "tasks/:id", to: "tasks#show", as: :task
-  delete "tasks/:id", to: "tasks#destroy"
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-
+  resources :tasks
 end
